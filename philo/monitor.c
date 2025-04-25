8bb53e1cea101eda8547257783306112ecce7723
Halil Albayrak <109541183+HalilAlb@users.noreply.github.com>
2025-04-25 16:20:33 +0300

    Update monitor.c tryed gettime

--- philo/monitor.c
+++ philo/monitor.c
@@ -50,3 +50,43 @@ void	*monitor_dinner(void *data)
 	}
 	return (NULL);
 }
+
+
+// static bool	philo_dead(t_philo *philo)
+// {
+// 	long	sed;
+// 	long	to_die;
+
+// 	if (get_bool(&philo->philo_mutex, &philo->full))
+// 		return (false);
+// 	sed = gettime(MILISECOND) - get_long(&philo->philo_mutex,
+// 			&philo->last_meal_time);
+// 	to_die = philo->table->time_to_die;
+// 	if (sed > to_die)
+// 		return (true);
+// 	return (false);
+// }
+
+// void	*monitor_dinner(void *data)
+// {
+// 	t_table	*table;
+// 	int		i;
+
+// 	table = (t_table *)data;
+// 	while (!all_threads_running(&table->table_mutex,
+// 			&table->threads_running_num, table->philo_num))
+// 		;
+// 	while (!simulations_fnished(table))
+// 	{
+// 		i = -1;
+// 		while (++i < table->philo_num && !simulations_fnished(table))
+// 		{
+// 			if (philo_dead(table->philos + i))
+// 			{
+// 				set_bool(&table->table_mutex, &table->end_simulation, true);
+// 				write_status(DIAD, table->philos + i, DEBUG_MOD);
+// 			}
+// 		}
+// 	}
+// 	return (NULL);
+// }
